651b57ae3ba74f83ebe3b0217b4f7a5136a7b948
heynah <hannanicol@gmail.com>
2016-02-01 19:46:27 +0000

    getting some data, add 'request' library, parse body to json access desired object properties ***** rounded to 2 places

--- node-code.js
+++ node-code.js
@@ -10,6 +10,8 @@ var request = require('request');
 request("http://api.open-notify.org/iss-now.json", function(err, res, body) {
     if (!err) {
         var issInfo = JSON.parse(body);
+        issInfo.iss_position.latitude = issInfo.iss_position.latitude.toFixed(2);
+        issInfo.iss_position.longitude = issInfo.iss_position.longitude.toFixed(2);
         
         console.log("The ISS is now at: " + issInfo.iss_position.latitude + " x " + issInfo.iss_position.longitude);
 
